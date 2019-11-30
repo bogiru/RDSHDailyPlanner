e4c91357ca73928549f9e3f6e83ce49905b02c17
bogiru <barbolinsemen2@gmail.com>
2019-11-30 17:11:36 +0300

    реализация recycleView в NoticeFragment

---
+++ app/src/main/java/com/bogiruapps/rdshapp/notice/NoticeAdapter.kt
@@ -0,0 +1,29 @@
+package com.bogiruapps.rdshapp.notice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bogiruapps.rdshapp.R
+import kotlinx.android.synthetic.main.notice_item.view.*
+
+class NoticeAdapter(private val notices: List<String>) : RecyclerView.Adapter<NoticeAdapter.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.notice_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = notices.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.view.notice_text_view.text = notices[position]
+    }
+
+
+class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+}
--- app/src/main/java/com/bogiruapps/rdshapp/notice/NoticeFragment.kt
+++ app/src/main/java/com/bogiruapps/rdshapp/notice/NoticeFragment.kt
@@ -2,18 +2,15 @@ package com.bogiruapps.rdshapp.notice
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.bogiruapps.rdshapp.R
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.fragment_notice_empty.*
+import kotlinx.android.synthetic.main.fragment_notice.view.*
 import kotlinx.android.synthetic.main.fragment_notice_empty.view.*
 
 class NoticeFragment : Fragment() {
@@ -26,7 +23,14 @@ class NoticeFragment : Fragment() {
         val args = NoticeFragmentArgs.fromBundle(arguments!!)
 
         if (args.hasSchool) {
+
+            val list = listOf<String>("tom", "red", "blue", "dsfdsfdsf", "dfdfggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggd", "rerytryt", "4543543534", "fgtruyiuyjh", "fghtfhgf", "dfdsfdsfsd")
+
             view = inflater.inflate(R.layout.fragment_notice, container, false)
+            val recyclerView = view.recycler_view_notices.apply {
+                layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false)
+                adapter = NoticeAdapter(list)
+            }
 
         }else {
             view = inflater.inflate(R.layout.fragment_notice_empty, container, false)
